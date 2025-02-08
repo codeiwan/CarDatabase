f973a7afbff4702b5836d661095ef3afcea397ca
codeiwan <pewlv7976@gmail.com>
2025-02-08 23:14:49 +0900

    CRUD Repository 생성 - 생성자 주입을 통한 CarRepository 주입 및 로거 추가 - save 메서드를 이용한 데이터 추가

--- cardatabase/src/main/java/com/iwdev/cardatabase/CardatabaseApplication.java
+++ cardatabase/src/main/java/com/iwdev/cardatabase/CardatabaseApplication.java
@@ -1,13 +1,35 @@
 package com.iwdev.cardatabase;
 
+import com.iwdev.cardatabase.domain.Car;
+import com.iwdev.cardatabase.domain.CarRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CardatabaseApplication {
+public class CardatabaseApplication implements CommandLineRunner {
+	private static final Logger logger = LoggerFactory.getLogger(CardatabaseApplication.class);
+
+	private final CarRepository repository;
+
+	public CardatabaseApplication(CarRepository repository) {
+		this.repository = repository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CardatabaseApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2023, 59000));
+		repository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2020, 29000));
+		repository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2022, 39000));
+
+		for (Car car : repository.findAll()) {
+			logger.info("brand: {}, model: {}", car.getBrand(), car.getModel());
+		}
+	}
 }
---
+++ cardatabase/src/main/java/com/iwdev/cardatabase/domain/CarRepository.java
@@ -0,0 +1,6 @@
+package com.iwdev.cardatabase.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CarRepository extends CrudRepository<Car, Long> {
+}
