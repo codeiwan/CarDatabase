ac0efbdcd1b2fd31402f5979e0d6d058a811652c
codeiwan <pewlv7976@gmail.com>
2025-02-11 18:07:45 +0900

    스프링 부트로 RESTful 웹 서비스 생성 - @RestController 애너테이션으로 RESTful 웹 서비스의 컨트롤러가 되도록 지정 - Owner 클래스의 cars 필드에 @JsonIgnore 이용

--- cardatabase/src/main/java/com/iwdev/cardatabase/domain/Owner.java
+++ cardatabase/src/main/java/com/iwdev/cardatabase/domain/Owner.java
@@ -1,10 +1,13 @@
 package com.iwdev.cardatabase.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 
 import java.util.List;
 
 @Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
 public class Owner {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -20,6 +23,7 @@ public class Owner {
         this.lastname = lastname;
     }
 
+    @JsonIgnore
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private List<Car> cars;
 
