3b7e83e188e7684f21b9296bf60920cbacf3a965
codeiwan <pewlv7976@gmail.com>
2025-02-18 23:20:15 +0900

    스프링 시큐리티 기능 구성 - SecurityFilterChain 빈으로 어느 경로가 보호되거나 보호되지 않는지 정의 - /login 엔드포인트에 대한 POST 메서드 요청을 인증 없이 허용 - 로그인 기능 테스트 확인

--- cardatabase/src/main/java/com/iwdev/cardatabase/SecurityConfig.java
+++ cardatabase/src/main/java/com/iwdev/cardatabase/SecurityConfig.java
@@ -3,12 +3,16 @@ package com.iwdev.cardatabase;
 import com.iwdev.cardatabase.service.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -21,6 +25,15 @@ public class SecurityConfig {
 
     public void configureGlobal (AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf((csrf) -> csrf.disable())
+                .sessionManagement((sessionManagement) -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+                        .requestMatchers(HttpMethod.POST, "/login").permitAll().anyRequest().authenticated());
+        return http.build();
     }
 
     @Bean
